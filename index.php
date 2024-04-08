1d8e1213b0d1115de3d8d1835614fd0c4b33db8e
DDorka02 <dobszaydorka@gmail.com>
2024-04-08 13:13:30 +0200

    php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+        include_once "Adatbazis.php";
+        ?>
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dobszay Dorka, Dolgozat</title>
+
+</head>
+<body>
+    <?php
+        
+    
+    
+    ?>
+</body>
+</html>
